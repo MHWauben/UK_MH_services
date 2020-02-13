8926a0cf5af93ed67a368d4be54e0b712034c9f4
Martine Wauben <martine.wauben@gmail.com>
2020-02-13 14:53:41 +0000

    Pull population per postcode for each country

---
+++ BACP_registered.R
@@ -0,0 +1,112 @@
+### Scrape BACP registered counsellors
+# Required libraries
+library(polite)
+library(rvest)
+library(purrr)
+library(stringr)
+library(dplyr)
+library(magrittr)
+library(downloader)
+library(readxl)
+
+# Pages to scrape
+pages <- seq(0, 37490, 10)
+
+log_start <- Sys.time()
+
+
+# Check that the robots.txt accepts scraping
+bacp_bow <- polite::bow(url = "https://www.bacp.co.uk/search/Register", 
+                        force = TRUE)
+
+# Scraping function to be mapped onto each page URL
+get_posts <- function(page, bow = bacp_bow){
+  print(paste0("https://www.bacp.co.uk/search/Register?skip=", page))
+  general <- polite::scrape(bacp_bow,
+                           query = list(skip = page)) %>%
+    rvest::html_node("main") %>%
+    rvest::html_node("form") %>%
+    rvest::html_nodes("div.template-search__body") %>%
+    rvest::html_nodes("div.template-search__results") %>%
+    rvest::html_nodes("article") 
+  
+  name_post <- general %>%
+    rvest::html_nodes("header") %>%
+    rvest::html_nodes("div.search-result__title") %>%
+    rvest::html_text()
+  # Split out names and locations - name comes first
+  nums <- 1:length(name_post)
+  names <- name_post[seq(1, length(nums), 2)]
+  # Remove backslash followed by r or n and multiple spaces
+  names <- gsub("(\\\r)|(\\\n)|(  )", "", names)
+  locations <- name_post[seq(2, length(nums), 2)]
+  postcodes <- stringr::str_extract(locations, "[A-Z]{1,3}[0-9]{1,3}[A-Z]*")  
+  
+  # Get accreditation status
+  member_type <- general %>%
+    rvest::html_nodes("div.search-result__body") %>%
+    rvest::html_nodes("div.search-result__status") %>%
+    rvest::html_attr("aria-label")
+  
+  result <- data.frame(names = names,
+                       locations = locations,
+                       postcodes = postcodes,
+                       member_type = member_type,
+                       stringsAsFactors = FALSE)
+  print(paste0(Sys.time() - log_start))
+  return(result)
+}
+
+# Get to each person's details - get name and postcode
+#result <- purrr::map_dfr(pages, ~get_posts(.x))
+#write.csv(result, "bacp_postcodes.csv")
+result_saved <- read.csv("bacp_postcodes.csv")
+
+
+# Count no per postcode
+postcode_count <- result_saved %>%
+  dplyr::filter(!is.na(postcodes)) %>%
+  dplyr::group_by(postcodes, member_type) %>%
+  dplyr::summarise(value = n()) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(value_sc = (value - min(value, na.rm = T)) / (max(value, na.rm = T) - min(value, na.rm = T)))
+
+
+### Add population per postcode ----
+## England & Wales
+# Source: 2011 census, NOMIS table KS101EW
+postcode_pop <- read.csv("https://www.nomisweb.co.uk/output/census/2011/Postcode_Estimates_Table_1.csv")
+postcode_pop$postcodes <- gsub(" +", "", gsub("[0-9][A-Z]{2}$", "", postcode_pop$Postcode))
+pop_totals <- postcode_pop %>%
+  dplyr::group_by(postcodes) %>%
+  dplyr::summarise(population = sum(Total)) %>%
+  dplyr::ungroup()
+## Scotland
+scot_pop <- read.csv("https://www.isdscotland.org/Products-and-Services/GPD-Support/Population/Census/_docs/2011/2011census_lc_pcsec2011_pop.csv?14:28:51")
+scot_pop$postcodes <- gsub(" .+", "", scot_pop$pcsec2011)
+scot_totals <- scot_pop %>%
+  dplyr::group_by(postcodes) %>%
+  dplyr::summarise(population = sum(Total)) %>%
+  dplyr::ungroup()
+## Northern Ireland
+downloader::download("https://www.ninis2.nisra.gov.uk/Download/Census%202011/Headcount%20and%20Household%20Estimates%20for%20Postcodes.zip", 
+         dest="ni_data.zip", mode="wb") 
+unzip("ni_data.zip")
+ni_pop <- readxl::read_excel("Headcount and Household Estimates for Postcodes.xlsx", 
+                     skip = 4,
+                     col_names = c("Postcode", "Total", "Males", "Females", "Households"),
+                     col_types = c("text", "numeric", "numeric", "numeric", "numeric")) %>%
+  dplyr::slice(., 1:(n()-9)) %>%
+  dplyr::mutate(Total = ifelse(is.na(Total), 0, Total))
+ni_pop$postcodes <- gsub(" ?[0-9][A-Z]{2}$", "", ni_pop$Postcode)
+ni_totals <- ni_pop %>%
+  dplyr::group_by(postcodes) %>%
+  dplyr::summarise(population = sum(Total)) %>%
+  dplyr::ungroup()
+
+postcode_totals <- rbind(rbind(pop_totals, scot_totals), ni_totals)
+
+
+### Combine data ----
+postcode_data <- dplyr::full_join(postcode_count, postcode_totals, by = "postcodes") %>%
+  dplyr::filter(!(is.na(postcodes)|is.na(population)))
