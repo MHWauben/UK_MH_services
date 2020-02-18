76fb623b96ba47f8381cb77c21707bd22064ea26
Martine Wauben <38880899+MHWauben@users.noreply.github.com>
2020-02-18 11:10:29 +0000

    Add leaflet map with members per capita on hover

--- BACP_registered.R
+++ BACP_registered.R
@@ -9,6 +9,7 @@ library(magrittr)
 library(downloader)
 library(readxl)
 library(sf)
+library(leaflet)
 
 # Pages to scrape
 pages <- seq(0, 37490, 10)
@@ -24,7 +25,7 @@ bacp_bow <- polite::bow(url = "https://www.bacp.co.uk/search/Register",
 get_posts <- function(page, bow = bacp_bow){
   print(paste0("https://www.bacp.co.uk/search/Register?skip=", page))
   general <- polite::scrape(bacp_bow,
-                           query = list(skip = page)) %>%
+                            query = list(skip = page)) %>%
     rvest::html_node("main") %>%
     rvest::html_node("form") %>%
     rvest::html_nodes("div.template-search__body") %>%
@@ -61,7 +62,7 @@ get_posts <- function(page, bow = bacp_bow){
 # Get to each person's details - get name and postcode
 #result <- purrr::map_dfr(pages, ~get_posts(.x))
 #write.csv(result, "bacp_postcodes.csv")
-result_saved <- read.csv("bacp_postcodes.csv")
+result_saved <- read.csv("https://raw.githubusercontent.com/MHWauben/UK_MH_services/population_counts/bacp_postcodes.csv")
 
 
 # Count no per postcode
@@ -90,12 +91,12 @@ scot_totals <- scot_pop %>%
   dplyr::ungroup()
 ## Northern Ireland
 downloader::download("https://www.ninis2.nisra.gov.uk/Download/Census%202011/Headcount%20and%20Household%20Estimates%20for%20Postcodes.zip", 
-         dest="ni_data.zip", mode="wb") 
+                     dest="ni_data.zip", mode="wb") 
 unzip("ni_data.zip")
 ni_pop <- readxl::read_excel("Headcount and Household Estimates for Postcodes.xlsx", 
-                     skip = 4,
-                     col_names = c("Postcode", "Total", "Males", "Females", "Households"),
-                     col_types = c("text", "numeric", "numeric", "numeric", "numeric")) %>%
+                             skip = 4,
+                             col_names = c("Postcode", "Total", "Males", "Females", "Households"),
+                             col_types = c("text", "numeric", "numeric", "numeric", "numeric")) %>%
   dplyr::slice(., 1:(n()-9)) %>%
   dplyr::mutate(Total = ifelse(is.na(Total), 0, Total))
 ni_pop$postcodes <- gsub(" ?[0-9][A-Z]{2}$", "", ni_pop$Postcode)
@@ -144,7 +145,7 @@ trans_cols <- c("memb_percap", "memb_percap_ra",
                 "memb_log", "memb_sqrt", "memb_cubrt",
                 "memb_percap_log", "memb_percap_sqrt", "memb_percap_cubrt")
 postcode_sc <- as.data.frame(apply(postcode_all[, trans_cols], 
-                     2, scale_vec))
+                                   2, scale_vec))
 colnames(postcode_sc) <- paste0(trans_cols, "_sc")
 postcode_all <- cbind(postcode_all, postcode_sc)
 
@@ -152,12 +153,27 @@ postcode_all <- cbind(postcode_all, postcode_sc)
 downloader::download("https://datashare.is.ed.ac.uk/bitstream/handle/10283/2597/GB_Postcodes.zip?sequence=1&isAllowed=y",
                      dest = "poly.zip", mode = "wb")
 unzip("poly.zip")
-postalDistrict <-  sf::st_read('GB_Postcodes/PostalDistrict.shp')
+postalDistrict <- sf::st_read('GB_Postcodes/PostalDistrict.shp')
 postalDistrict$postcodes <- postalDistrict$PostDist
+postalDistrict$geometry <- sf::st_transform(postalDistrict$geometry, crs = "+proj=longlat +datum=WGS84")
 
 # Merge and visualise data
 postcode_poly <- st_as_sf(merge(postcode_all, postalDistrict))
 
-plot(postcode_poly["memb_sqrt_sc"], lty = 0)
+# Number of members per capita (cube root & scaled)
 plot(postcode_poly["memb_percap_cubrt_sc"], lty = 0)
+# Number of members per capita (rank & scaled)
 plot(postcode_poly["memb_percap_ra_sc"], lty = 0)
+
+# Do the same with leaflet for interactivity
+pal <- colorNumeric(
+  palette = "YlGnBu",
+  domain = postcode_poly$memb_percap_cubrt_sc)
+
+leaflet(postcode_poly) %>%
+  addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
+  addPolygons(label = ~memb_percap,
+              color = ~pal(memb_percap_cubrt_sc), weight = 1, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.5, 
+              highlightOptions = highlightOptions(color = "white", weight = 1, bringToFront = TRUE))
+
